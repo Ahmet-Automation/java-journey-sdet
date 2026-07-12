37e54d8511e85995a00bc516d34cb5c5bb173d77
Ahmet T <ahmet.qa.at@gmail.com>
2026-07-12 23:30:05 +0200

    feat(java): ConstructorIntro.java

---
+++ src/test/java/com/donau/class_and_object/Student.java
@@ -0,0 +1,12 @@
+package com.donau.class_and_object;
+
+public class Student {  // default constructor
+
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
