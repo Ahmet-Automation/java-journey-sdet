3358f01d95f590b296551d3ea7c2f75d4bb5013b
Ahmet T <ahmet.qa.at@gmail.com>
2026-07-12 23:29:57 +0200

    feat(java): ConstructorIntro.java

---
+++ src/test/java/com/donau/class_and_object/StudentClient.java
@@ -0,0 +1,15 @@
+package com.donau.class_and_object;
+
+public class StudentClient {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("James", 18);
+
+//        student1.name = "James";
+//        student1.age = 18;
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+    }
+
+}
